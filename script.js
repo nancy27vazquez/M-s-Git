b7f41f386c1afc56221faa2744361f89e692682b
nancy27vazquez <nevadg@outlook.com>
2019-08-10 12:05:24 -0500

    Login main structure

---
+++ script.js
@@ -0,0 +1,25 @@
+// git init
+
+// git status
+
+// git add <file>
+// git add .                 únicamente desde donde estoy parado
+// git add -A                guarda todo sin importar ubicación
+
+// git rm --cached  <file>   dejar de trackear x archivo
+
+// git commit -m"<message>"
+
+// git branch develop        crear rama de desarrollo
+
+// git branch                consultar ramas
+
+// git branch -d <file>      borrar rama
+
+// git checkout <branch>     cambio de rama
+
+/*==== DEVELOP ES LA RAMA PRINCIPAL ==== */
+
+// git branch feature/<name>   crear nueva branch con convención feature (standard)
+
+/*==== TRABAJAR EN LA FEATURE ==== */
