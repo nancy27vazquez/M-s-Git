ba17cd591b83677ceebe1ff8eee04615cab4c74c
nancy27vazquez <nevadg@outlook.com>
2019-08-10 12:46:49 -0500

    changes

---
+++ script.js
@@ -0,0 +1,35 @@
+// git init
+
+// git status
+
+// git add <file>
+// git add .                 únicamente desde donde estoy parado
+// git add -A                guarda todo sin importar ubicación
+
+// git rm --cached  <file>   dejar de trackear x archivo
+
+// git commit -m"<message>"
+
+// git branch develop        crear rama de desarrollo
+
+// git branch                consultar ramas
+
+// git branch -d <file>      borrar rama
+
+// git checkout <branch>     cambio de rama
+
+/*==== DEVELOP ES LA RAMA PRINCIPAL ==== */
+
+// git branch feature/<name>   crear nueva branch con convención feature (standard)
+
+/*==== TRABAJAR EN LA FEATURE ==== */
+
+// En github crear repo remoto
+
+// git remote add origin <link>
+
+// git remote  -v         enlistar repos remotos
+
+// EN github, pull request HACIA DEVELOPMENT
+
+//
